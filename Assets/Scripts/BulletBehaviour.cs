4584b5bebb01cc5d53b05633b2ec472047592f1d
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2020-05-22 12:58:53 +0200

    Status at lunch - gun is shooting and teleport is happening

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+
+    public float initialForce;
+
+    public Transform playerTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * initialForce, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Teleport") {
+            playerTransform.position = collision.GetContact(0).point;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+
+    public Transform bulletOrigin;
+
+    public Transform playerRoot;
+
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject bullet = Instantiate(bulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
+            bullet.GetComponent<BulletBehaviour>().playerTransform = playerRoot;
+        }
+    }
+}
